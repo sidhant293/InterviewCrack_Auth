8a47c2dbbb2371995e90556ed65b22e53c1b1f28
Sidhant <56150322+sidhant293@users.noreply.github.com>
2022-01-24 22:32:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,5 +10,5 @@ app.get('/',(req,res)=>{
     res.send(`InterviewCrack AuthServer Running`);
 })
 
-const port=process.env.port || 5000;
+const port=process.env.PORT || 5000;
 app.listen(port,()=>console.log(`Listening to port ${port}`));
