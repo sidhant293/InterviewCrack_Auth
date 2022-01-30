0f55b91ca2f156a98fedf17b960720573d128e38
Sidhant <56150322+sidhant293@users.noreply.github.com>
2022-01-30 15:27:12 +0530

    cors added

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express=require('express');
+const cors = require('cors');
 const app=express();
 const validation=require('./validation');
 
 const port=process.env.PORT || 5000;
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/validate',validation);
